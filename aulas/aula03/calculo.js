7610816ded0416296dfe45f4ebde5edf248e1f45
Matheus <matheus.valerio6@etec.sp.gov.br>
2024-09-02 18:27:45 -0300

    Introdução ao Java Script

---
+++ aulas/aula03/calculo.js
@@ -0,0 +1,15 @@
+function somar(){
+    var x = 2
+    var y = 2
+    let res = x+y
+    document.getElementById("soma").innerHTML=res
+    document.getElementById("conf-soma").innerHTML="Calculado ✔"
+}
+
+function subtrair(){
+    var x = 2
+    var y = 2
+    let res = x-y
+    document.getElementById("subtração").innerHTML=res
+    document.getElementById("conf-subtração").innerHTML="Calculado ✔"
+}
